1cc43ea44b66c7b3a309ee88813859c4837f08f2
LAPTOP-9F4GMIKT\ASUS <huyblink123@gmail.com>
2021-10-08 21:18:32 +0700

    First Commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChanhThuStore.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChanhThuStore.Startup))]
+namespace ChanhThuStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
